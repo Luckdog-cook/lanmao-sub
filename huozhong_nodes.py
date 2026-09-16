bca0ab395a32aeb6cdd3d1fb1adb5b73e9b65020
Luckdog-cook <8885609@gmail.com>
2026-09-16 23:23:20 +0800

    Update huozhong_nodes.py

--- huozhong_nodes.py
+++ huozhong_nodes.py
@@ -46,7 +46,7 @@ BASE_HEADERS = {
     "X-App-Version": "1.1.21",
     "X-Device-OS": "Android",
     "Accept": "application/json",
-    "Accept-Encoding": "gzip",
+    "Accept-Encoding": "q6i3q78",
 }
 
 _global_session = requests.Session()
